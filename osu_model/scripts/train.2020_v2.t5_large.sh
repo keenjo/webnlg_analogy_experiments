0f52e867943d52936ef66db6c8465f3590ef8f9a
keenjo <90385673+keenjo@users.noreply.github.com>
2022-07-25 12:01:44 +0200

    code and data for osu model

---
+++ osu_model/scripts/train.2020_v2.t5_large.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd $(dirname $0)/..
+
+export CUDA_VISIBLE_DEVICES=0
+data=2021_v3_en
+model=t5_large
+SAVEDIR=osu_model_checkpoints/$data.$model
+CACHEDIR=osu_model_cache/$model
+
+mkdir -p $SAVEDIR
+mkdir -p $CACHEDIR
+
+python finetune-transformers/train.py \
+  --pretrained-model-path "t5-large" \
+  --train-source-data-path $(readlink -f "data-prep/$data/train.mr") \
+  --train-target-data-path $(readlink -f "data-prep/$data/train.lx") \
+  --valid-source-data-path $(readlink -f "data-prep/$data/valid.mr") \
+  --valid-target-data-path $(readlink -f "data-prep/$data/valid.lx") \
+  --save-dir $(readlink -f $SAVEDIR) \
+  --cache-dir $(readlink -f $CACHEDIR) \
+  --max-epoch 500 --patience 10 \
+  --batch-size 8 --update-frequency 1 \
+  --learning-rate 2e-5 \
+  --valid-batch-size 8 \
+  --valid-bleu --valid-beam-size 5 --valid-max-length 200
+
+  # Took this out of the arguments because this file was not created in preprocessing and a 'None' value is acceptable here according to train.py
+  #--indivisible-tokens-path $(readlink -f "data-prep/$data/indivisible_tokens.txt") \
